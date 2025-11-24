6c963819dc929a3a99ff82c35873040580e2cbec
Sagar kumar <panditsagar4995@gmail.com>
2025-11-24 17:13:29 +0530

    c

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+
+
+corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth.routes"));
